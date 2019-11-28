c3be58dc747658df535d28ae7944892d5d7eda42
Meynor R Lucas <andre.rlucas@outlook.com>
2019-11-28 17:24:29 -0600

    adding content

---
+++ content/_index.md
@@ -0,0 +1,7 @@
+---
+title: "Home"
+date: 2019-11-28T01:57:31-06:00
+draft: true
+css: ["main.css"]
+js: ["main.js"]
+---
---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+console.log('This file has been read well. Yup...')
+
+function getPostItem(id) {
+  window.location.replace(`/posts/${id}`)
+}
